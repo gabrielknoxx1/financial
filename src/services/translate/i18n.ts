ada181c52f379d1856fd41567c998df2329e3465
gabrielknoxx1 <byelg.sousa@gmail.com>
2021-10-01 17:48:30 -0300

    request to repositories to strings

--- src/services/translate/i18n.ts
+++ src/services/translate/i18n.ts
@@ -1,24 +1,39 @@
 import i18n from 'i18next';
 import {initReactI18next} from 'react-i18next';
+import ChainedBackend from 'i18next-chained-backend';
+import HttpBackend from 'i18next-http-backend';
+import resourcesToBackend from 'i18next-resources-to-backend';
 import {languages} from './languages';
 
-// the translations
-// (tip move them in a JSON file and import them,
-// or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
 const resources = {
   en: {
     translation: languages.en,
   },
-  pt: {
-    translation: languages.pt,
-  },
 };
-
 i18n
+  .use(ChainedBackend)
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
+    fallbackLng: 'en',
+    lng: 'en',
     resources,
-    lng: 'en', // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
+    backend: {
+      backends: [
+        HttpBackend,
+        resourcesToBackend((lng, ns, clb) => {
+          import(`./locales/${lng}/${ns}.json`)
+            .then(resources => clb(null, resources))
+            .catch(clb);
+        }),
+      ],
+      backendOptions: [
+        {
+          loadPath:
+            'https://localise.biz:443/api/export/all.json?key=6_wMa_dOKYbs0kghT-qkH-f5nV-HvBOP',
+        },
+      ],
+    },
+    // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
     // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
     // if you're using a language detector, do not define the lng option
 
